b0809f7893872aaf821077913a5b445d1f8c2f5f
CarlosIbanezLopez <ingenierosistemasacademico@gmail.com>
2023-10-17 13:53:46 -0400

    primera

---
+++ src/keys.js
@@ -0,0 +1,13 @@
+//dir de la db
+module.exports = {
+
+    database: {
+        connectionLimit: 100,
+        host: 'containers-us-west-183.railway.app',//'localhost',//'152.70.216.169',//'localhost', //'',
+        port: '6839',//'3306',
+        user: 'root',//'root',//'harold151199',//'harold',
+        password: 'password',//'i86kGJkkYoWGVsLx1zbb',//'123456789',//'151199',
+        database: 'pruebaC',//'sw1pizarrac4'
+        debug: false
+    }
+}
